f274c61890e6a81c512d5ed66e8f3cc1b384d739
NetAcad DEVASC <changeme@example.com>
2024-07-11 18:26:46 +0000

    git add ListaDeIntegrantes.py archivo de item 1

---
+++ ListaDeIntegrantes.py
@@ -0,0 +1,6 @@
+nombres = ["Benjamin", "Guillermo"]
+apellidos = ["Guerrero", "Curiqueo"]
+
+for i in range(len(nombres)):
+    nombre_completo = nombres[i] + "" + apellidos[i]
+    print (nombre_completo)
